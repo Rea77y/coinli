e2bfc085ccdf027ee2c22782e15497436bfe4c30
brain <braintime132@gmail.com>
2020-12-20 15:35:29 +0200

    sadasd

--- app/src/main/java/com/example/coinli/Profile.java
+++ app/src/main/java/com/example/coinli/Profile.java
@@ -1,4 +1,41 @@
 package com.example.coinli;
 
-public class Profile {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+
+        View sign_in_button= findViewById(R.id.profile_home_button);
+        sign_in_button.setOnClickListener((View.OnClickListener) this);
+        View sign_up_button= findViewById(R.id.profile_algorithm_button);
+        sign_up_button.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick(View arg0) {
+        if(arg0.getId() == R.id.profile_home_button) {
+            //Transports users to the home activity
+            Intent transfer_to_sign_in = new Intent(this, Home.class);
+            this.startActivity(transfer_to_sign_in);
+        }
+        if(arg0.getId() == R.id.profile_algorithm_button)
+        {
+            Intent transfer_to_sign_up = new Intent(this, Sign_up.class);
+            this.startActivity(transfer_to_sign_up);
+        }
+    }
+
+
+
+
 }
+
