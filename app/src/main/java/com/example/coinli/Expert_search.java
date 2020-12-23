0077205525a13cf083c039b03ff84bfd3ddaa759
brain <braintime132@gmail.com>
2020-12-23 20:40:12 +0200

    sadasd

---
+++ app/src/main/java/com/example/coinli/Expert_search.java
@@ -0,0 +1,19 @@
+package com.example.coinli;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class Expert_search {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_expert_search);
+        }
+
+}
