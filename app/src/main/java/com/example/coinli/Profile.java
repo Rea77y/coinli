242024993eea6d364c29820d2781afa60155b0ca
ellay <elay.perets@gmail.com>
2020-12-30 12:54:57 +0200

    we are fucked

--- app/src/main/java/com/example/coinli/Profile.java
+++ app/src/main/java/com/example/coinli/Profile.java
@@ -14,12 +14,17 @@ public class Profile extends AppCompatActivity implements View.OnClickListener  
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
-
+        View profile_settings_button = findViewById(R.id.profile_settings_button);
+        profile_settings_button.setOnClickListener(this);
 
     }
 
     public void onClick(View arg0) {
-
+        if(arg0.getId() == R.id.profile_settings_button)
+        {
+            Intent transfer_to_settings = new Intent(this,Settings.class);
+            this.startActivity(transfer_to_settings);
+        }
     }
 
 
