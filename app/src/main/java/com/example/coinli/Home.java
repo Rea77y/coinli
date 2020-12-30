5d2e58e10de79749f2ac660d7e2c9c515d0a54c2
ellay <elay.perets@gmail.com>
2020-12-30 11:39:10 +0200

    we are fucked

--- app/src/main/java/com/example/coinli/Home.java
+++ app/src/main/java/com/example/coinli/Home.java
@@ -21,13 +21,50 @@ public class Home extends AppCompatActivity implements View.OnClickListener {
 
         View home_chat_community_button = findViewById(R.id.home_chat_button);
         home_chat_community_button.setOnClickListener(this);
+        View home_experts_button = findViewById(R.id.home_experts_button);
+        home_experts_button.setOnClickListener(this);
+       // View home_tips_button = findViewById(R.id.home_tips_button);
+       // home_tips_button.setOnClickListener(this);
+        View home_balance_button = findViewById(R.id.home_balance_button);
+        home_balance_button.setOnClickListener(this);
+        View home_settings_button = findViewById(R.id.home_settings_button);
+        home_settings_button.setOnClickListener(this);
+        View home_profile_button = findViewById(R.id.home_profile_button);
+        home_profile_button.setOnClickListener(this);
+
     }
 
     public void onClick(View arg0) {
-        if(arg0.getId() == R.id.home_chat_button) {
-            //Transports users to the home activity
+        if(arg0.getId() == R.id.home_experts_button)
+        {
+            Intent transfer_to_experts = new Intent(this,Experts.class);
+            this.startActivity(transfer_to_experts);
+        }
+        /*if(arg0.getId() == R.id.home_tips_button)
+        {
             Intent transfer_to_ChatCommunity = new Intent(this,ChatCommunity.class);
             this.startActivity(transfer_to_ChatCommunity);
+        }
+        */
+        if(arg0.getId() == R.id.home_balance_button)
+        {
+            Intent transfer_to_balance = new Intent(this,Balance.class);
+            this.startActivity(transfer_to_balance);
+        }
+        if(arg0.getId() == R.id.home_chat_button)
+        {
+            Intent transfer_to_ChatCommunity = new Intent(this,ChatCommunity.class);
+            this.startActivity(transfer_to_ChatCommunity);
+        }
+        if(arg0.getId() == R.id.home_settings_button)
+        {
+            Intent transfer_to_settings = new Intent(this,Settings.class);
+            this.startActivity(transfer_to_settings);
+        }
+        if(arg0.getId() == R.id.home_profile_button) {
+            //Transports users to the home activity
+            Intent transfer_to_profile = new Intent(this,Profile.class);
+            this.startActivity(transfer_to_profile);
         }
 
     }
