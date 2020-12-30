f02de7703dcbc7f87d432a9503f2e0772f8a5e14
brain <braintime132@gmail.com>
2020-12-30 23:20:12 +0200

    sadasd

--- app/src/main/java/com/example/coinli/Personal_info.java
+++ app/src/main/java/com/example/coinli/Personal_info.java
@@ -14,16 +14,24 @@ public class Personal_info extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personal_info);
-        View profile_settings_button = findViewById(R.id.profile_settings_button);
-        profile_settings_button.setOnClickListener(this);
+        View personal_info_back_button = findViewById(R.id.personal_info_back_button);
+        personal_info_back_button.setOnClickListener(this);
+        View personal_info_edit_button = findViewById(R.id.personal_info_edit_button);
+        personal_info_edit_button.setOnClickListener(this);
+
 
     }
 
     public void onClick(View arg0) {
-        if(arg0.getId() == R.id.profile_settings_button)
+        if(arg0.getId() == R.id.personal_info_back_button)
         {
-            Intent transfer_to_settings = new Intent(this,Settings.class);
-            this.startActivity(transfer_to_settings);
+            //make option to edit
+            Intent transfer_to_profile = new Intent(this,Profile.class);
+            this.startActivity(transfer_to_profile);
+        }
+        if(arg0.getId() == R.id.personal_info_edit_button)
+        {
+            //make option to edit
         }
     }
 
