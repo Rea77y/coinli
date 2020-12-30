8902d8e52c3c187a1fbd0f44c9a43120eee494fd
brain <braintime132@gmail.com>
2020-12-30 23:22:29 +0200

    sadasd

--- app/src/main/java/com/example/coinli/Profile.java
+++ app/src/main/java/com/example/coinli/Profile.java
@@ -16,6 +16,8 @@ public class Profile extends AppCompatActivity implements View.OnClickListener  
         setContentView(R.layout.profile);
         View profile_settings_button = findViewById(R.id.profile_settings_button);
         profile_settings_button.setOnClickListener(this);
+        View profile_personal_info_button = findViewById(R.id.profile_personal_information_button);
+        profile_personal_info_button.setOnClickListener(this);
 
     }
 
@@ -24,6 +26,11 @@ public class Profile extends AppCompatActivity implements View.OnClickListener  
         {
             Intent transfer_to_settings = new Intent(this,Settings.class);
             this.startActivity(transfer_to_settings);
+        }
+        if(arg0.getId() == R.id.profile_personal_information_button)
+        {
+            Intent transfer_to_personal_info = new Intent(this,Personal_info.class);
+            this.startActivity(transfer_to_personal_info);
         }
     }
 
