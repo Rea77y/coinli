8fd0c19776ff9af6038ebfb0184d1c9d2b215b3f
brain <braintime132@gmail.com>
2021-01-11 17:17:09 +0200

    sadasd

--- app/src/main/java/com/example/coinli/Experts.java
+++ app/src/main/java/com/example/coinli/Experts.java
@@ -13,20 +13,45 @@ public class Experts extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.experts);
 
+        View experts_chat_community_button = findViewById(R.id.experts_chat_button);
+        experts_chat_community_button.setOnClickListener(this);
+        View experts_balance_button = findViewById(R.id.experts_balance_button);
+        experts_balance_button.setOnClickListener(this);
+        View experts_settings_button = findViewById(R.id.experts_settings_button);
+        experts_settings_button.setOnClickListener(this);
+        View experts_home_button = findViewById(R.id.experts_home_button);
+        experts_home_button.setOnClickListener(this);
+        View experts_profile_button = findViewById(R.id.experts_profile_button);
+        experts_profile_button.setOnClickListener(this);
+        View experts_findexperts_button = findViewById(R.id.experts_findexperts_button);
+        experts_findexperts_button.setOnClickListener(this);
     }
     public void onClick(View arg0) {
-        if (arg0.getId() == R.id.sign_in_sign_in_button) {
-            //Transports users to the home activity
-            Intent transfer_to_sign_in = new Intent(this, Home.class);
-            this.startActivity(transfer_to_sign_in);
+        if (arg0.getId() == R.id.experts_settings_button) {
+            //Transports users to the settings activity
+            Intent experts_settings_button = new Intent(this, Settings.class);
+            this.startActivity(experts_settings_button);
         }
-        if (arg0.getId() == R.id.sign_in_sign_up_button) {
-            Intent transfer_to_sign_up = new Intent(this, Sign_up.class);
-            this.startActivity(transfer_to_sign_up);
+        if (arg0.getId() == R.id.experts_chat_button) {
+            Intent transfer_to_chat = new Intent(this, ChatCommunity.class);
+            this.startActivity(transfer_to_chat);
         }
-        if (arg0.getId() == R.id.sign_in_forgot_password_button) {
-            Intent transfer_to_sign_up = new Intent(this, Sign_up.class);
-            this.startActivity(transfer_to_sign_up);
+        if (arg0.getId() == R.id.experts_home_button) {
+            Intent transfer_to_home = new Intent(this, Home.class);
+            this.startActivity(transfer_to_home);
         }
+        if (arg0.getId() == R.id.experts_balance_button) {
+            Intent transfer_to_balance = new Intent(this, Balance.class);
+            this.startActivity(transfer_to_balance);
+        }
+        if (arg0.getId() == R.id.experts_profile_button) {
+            Intent transfer_to_profile = new Intent(this, Profile.class);
+            this.startActivity(transfer_to_profile);
+        }
+        if (arg0.getId() == R.id.experts_findexperts_button) {
+            Intent transfer_to_findExperts = new Intent(this, ExpertSearch.class);
+            this.startActivity(transfer_to_findExperts);
+        }
+
     }
 }
--- app/src/main/java/com/example/coinli/User_info.java
+++ app/src/main/java/com/example/coinli/User_info.java
@@ -1,4 +1,40 @@
 package com.example.coinli;
 
-public class User_info {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class User_info extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_info);
+        View user_info_settings_button = findViewById(R.id.user_info_settings_button);
+        user_info_settings_button.setOnClickListener(this);
+        View user_info_back_button = findViewById(R.id.user_info_back_button);
+        user_info_back_button.setOnClickListener(this);
+    }
+
+    public void onClick(View arg0) {
+        if (arg0.getId() == R.id.user_info_settings_button) {
+            //Transports users to the settings activity
+            Intent transfer_to_settings = new Intent(this, Settings.class);
+            this.startActivity(transfer_to_settings);
+        }
+        if (arg0.getId() == R.id.user_info_back_button) {
+            Intent transfer_to_Chat_private = new Intent(this, Chat_private.class);
+            this.startActivity(transfer_to_Chat_private);
+        }
+
+    }
+
+
+
+
+
+
 }
