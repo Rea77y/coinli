7ad64f83e886211108666a02ad76712fe5a7a81b
Niv Burlak <nivburlak@gmail.com>
2021-01-14 12:50:01 +0200

    finish login and sign up

--- app/src/main/java/com/example/coinli/MainActivity.java
+++ app/src/main/java/com/example/coinli/MainActivity.java
@@ -1,42 +1,71 @@
 package com.example.coinli;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText  username,  password;
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseAuth  mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        View sign_in_button= findViewById(R.id.sign_in_sign_in_button);
-        sign_in_button.setOnClickListener(this);
-        View sign_up_button= findViewById(R.id.sign_in_sign_up_button);
-        sign_up_button.setOnClickListener(this);
-        View forgot_password= findViewById(R.id.sign_in_forgot_password_button);
-        forgot_password.setOnClickListener(this);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        username  = findViewById(R.id.editTextTextPersonName);
+        password = findViewById(R.id.editTextTextPassword);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if(mFirebaseUser != null)
+                {
+                    Toast.makeText(MainActivity.this, "You are logged in", Toast.LENGTH_SHORT).show();
+                    Intent t = new Intent(MainActivity.this, Home.class);
+                    startActivity(t);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Please login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+
+
+
+
+
     }
 
     public void onClick(View arg0) {
-        if(arg0.getId() == R.id.sign_in_sign_in_button) {
-            //Transports users to the home activity
-            Intent transfer_to_sign_in = new Intent(this, Home.class);
-            this.startActivity(transfer_to_sign_in);
-        }
-        if(arg0.getId() == R.id.sign_in_sign_up_button)
-        {
-            Intent transfer_to_sign_up = new Intent(this, Sign_up.class);
-            this.startActivity(transfer_to_sign_up);
-        }
-        if(arg0.getId() == R.id.sign_in_forgot_password_button)
-        {
-            Intent transfer_ForgotPassword = new Intent(this, ForgotPassword.class);
-            this.startActivity(transfer_ForgotPassword);
-        }
+
     }
 
 
@@ -51,5 +80,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //verify data
         Intent t = new Intent(this, Home.class);
         startActivity(t);
+    }
+    private void signIn() {
+        String usernameS = username.getText().toString();
+        String passwordS = password.getText().toString();
+        if(usernameS.isEmpty())
+        {
+            username.setError("Please enter a username");
+            username.requestFocus();
+        }
+        else if (passwordS.isEmpty())
+        {
+            password.setError("Please enter a password");
+            password.requestFocus();
+        }
+        else if(usernameS.isEmpty()&&passwordS.isEmpty())
+        {
+            Toast.makeText(this, "The fields are empty", Toast.LENGTH_SHORT).show();
+        }
+        else if(!(usernameS.isEmpty()&&passwordS.isEmpty()))
+        {
+            mFirebaseAuth.signInWithEmailAndPassword(usernameS,passwordS).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful())
+                    {
+                        Toast.makeText(MainActivity.this, "Login Error, please try again", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Intent t = new Intent(MainActivity.this, Home.class);
+                        startActivity(t);
+                    }
+                }
+            });
+
+        }
+        else
+        {
+            Toast.makeText(MainActivity.this, "Error occurred", Toast.LENGTH_SHORT).show();
+        }
+
+
+        //Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        //short RC_SIGN_IN = 0;
+        //startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    public void GoSignUp(View view) {
+        Intent transfer_to_sign_up = new Intent(this, Sign_up.class);
+        this.startActivity(transfer_to_sign_up);
     }
 }
--- app/src/main/java/com/example/coinli/Sign_up.java
+++ app/src/main/java/com/example/coinli/Sign_up.java
@@ -1,29 +1,79 @@
 package com.example.coinli;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Sign_up extends AppCompatActivity {
+
+    EditText username,  password;
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        username = findViewById(R.id.email2);
+        password = findViewById(R.id.password2);
+
 
     }
 
     public void save_details(View view) {
-        //to do data base and save data
-        //if valid close intend
-        finish();
+        String usernameS = username.getText().toString();
+        String passwordS = password.getText().toString();
+        if(usernameS.isEmpty())
+        {
+            username.setError("Please enter a username");
+            username.requestFocus();
+        }
+        else if (passwordS.isEmpty())
+        {
+            password.setError("Please enter a password");
+            password.requestFocus();
+        }
+        else if(usernameS.isEmpty()&&passwordS.isEmpty())
+        {
+            Toast.makeText(this, "The fields are empty", Toast.LENGTH_SHORT).show();
+        }
+        else if(!(usernameS.isEmpty()&&passwordS.isEmpty()))
+        {
+            mFirebaseAuth.createUserWithEmailAndPassword(usernameS,passwordS).addOnCompleteListener(Sign_up.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task)
+                {
+                    if(!task.isSuccessful())
+                    {
+                        Toast.makeText(Sign_up.this, "Signup unsuccessful, please try again", Toast.LENGTH_SHORT).show();
+
+                    }
+                    else
+                    {
+                        startActivity(new Intent(Sign_up.this, Home.class));
+                    }
+                }
+            });
+        }
+        else
+        {
+            Toast.makeText(Sign_up.this, "Error occurred", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
