34d2b5b8d764fee735a874bde0de3df5c4b1ed78
Niv Burlak <nivburlak@gmail.com>
2021-01-15 03:44:47 +0200

    finish forgot password, you should check it, im proud of it

--- app/src/main/java/com/example/coinli/MainActivity.java
+++ app/src/main/java/com/example/coinli/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.coinli;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,6 +16,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     EditText  username,  password;
     GoogleSignInClient mGoogleSignInClient;
     FirebaseAuth  mFirebaseAuth;
+    Button forgot;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
@@ -37,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         username  = findViewById(R.id.editTextTextPersonName);
         password = findViewById(R.id.editTextTextPassword);
+        forgot = findViewById(R.id.sign_in_forgot_password_button);
 
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
 
@@ -82,6 +89,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(t);
     }
     private void signIn() {
+
+
+        //Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        //short RC_SIGN_IN = 0;
+        //startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    public void GoSignUp(View view) {
+        Intent transfer_to_sign_up = new Intent(this, Sign_up.class);
+        this.startActivity(transfer_to_sign_up);
+    }
+
+    public void signIn(View view) {
         String usernameS = username.getText().toString();
         String passwordS = password.getText().toString();
         if(usernameS.isEmpty())
@@ -121,14 +141,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(MainActivity.this, "Error occurred", Toast.LENGTH_SHORT).show();
         }
 
-
-        //Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        //short RC_SIGN_IN = 0;
-        //startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
-    public void GoSignUp(View view) {
-        Intent transfer_to_sign_up = new Intent(this, Sign_up.class);
-        this.startActivity(transfer_to_sign_up);
+    public void resetPassword(View view) {
+        final EditText resetMail = new EditText(view.getContext());
+        AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(view.getContext());
+        passwordResetDialog.setTitle("Reset password?");
+        passwordResetDialog.setMessage("Enter Your Email To Receive Reset Link.");
+        passwordResetDialog.setView(resetMail);
+
+        passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String Mail = resetMail.getText().toString();
+                mFirebaseAuth.sendPasswordResetEmail(Mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Reset Link Was Sent To Your Mail", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error! Reset Link Was Not Sent", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }) ;
+        passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+
+            }
+        });
+
     }
 }
